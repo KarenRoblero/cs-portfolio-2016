c7d74ca50cac1107d573513058e75d74bbc6c53a
Karen Roblero <kroblero.sh@mysummitps.org>
2017-03-10 20:50:43 +0000

    Done

--- calculator.js
+++ calculator.js
@@ -1,17 +1,17 @@
-//
+//Defining Variables 
 var num1Input = document.getElementById("num1Input");
 var operator = document.getElementById("operator");
 var input2 = document.getElementById("input2");
 var calculatorbutton = document.getElementById("calculatorbutton");
 
-//
+// I start the function
 function solve() {
-    //
+    // I further define the variables specific to this function
     var num1 = num1Input.value; 
     var num2 = input2.value; 
     var answer = document.getElementById("answer");
     
-    //
+    // I use if statements to say when the dropdown is a certain value, to do a certain operation to the two inputed numbers
     if (operator.value === "+") {
         answer.innerHTML = Number(num1) + Number(num2);
     }
@@ -32,5 +32,5 @@ function solve() {
     }
 }
 
-//
+// I tell the computer to run the function when it "hears" the click of the calculator button with the addEventListener function
 calculatorbutton.addEventListener("click", solve);
--- todo-list.js
+++ todo-list.js
@@ -1,16 +1,44 @@
-var num1input = document.getElementById ("num1input")
-var select = document.getElementById ("select")
-var output = document.getElementById ("output")
-var inputButton = document.getElementById ("inputButton")
+//define all variableds
+var num1input = document.getElementById ("num1input");
+var select = document.getElementById("select");
+var urgent = document.getElementById("urgentthingsdiv");
+var mustdo = document.getElementById("mustdodiv");
+var shoulddo = document.getElementById("shoulddodiv");
+var inputButton = document.getElementById("inputButton");
+var output = document.getElementById("output");
+var all = document.getElementById("all");
 
+//says when input Button is clicked, it runs the function where data inputed into the num1input slot is added
 inputButton.addEventListener ("click", function (program) {
     
     var task = "<p>" + num1input.value + "<p>" + "<br>";
-    output.innerHTML = output.innerHTML + task; 
 
-}); 
-all.addeventListener("click", function (evt) {
+//this is where it shows the location of where the data is added; I used if statements with the drop down as the value put
+//in with the drop down determines the location
+if (select.value == "urgent") {
+    urgent.innerHTML += num1input.innerHTML + task;
+}
+
+else if(select.value == "mustdo") {
+    mustdo.innerHTML = num1input.innerHTML + task;
+}
+
+else if(select.value == "shoulddo") {
+    shoulddo.innerHTML = num1input.innerHTML + task;
+}
+
+ output.addEventListener("click", function (evt) {
+     var targettext=evt.target;
+     targettext.style.textDecoration = "line-through";
+     
+ });
+ 
+});
+//I tell the computer to listen for a click on any data in the all div and say that if it hears it, it should add a line-through
+all.addEventListener("click", function (evt) {
     var targettext=evt.target;
     targettext.style.textDecoration = "line-through";
-}); 
+    
+});
+
 
